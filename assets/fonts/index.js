05cc5083ce0c4bbdb0adbb327759812106ec87a2
Erik Vänttinen <eepi1994@hotmail.com>
2023-04-04 12:26:56 +0300

    Fonts and typography

--- assets/fonts/index.js
+++ assets/fonts/index.js
@@ -3,3 +3,12 @@
  */
 
 import './fonts';
+
+// Font families
+import './Adelon-Bold.ttf';
+import './Adelon-Medium.ttf';
+import './Adelon-Regular.ttf';
+import './RobotoSlab-Regular.ttf';
+import './RobotoSlab-Medium.ttf';
+import './RobotoSlab-SemiBold.ttf';
+import './RobotoSlab-Bold.ttf';
