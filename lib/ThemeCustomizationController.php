b473709ed7e3a1999c45de87a562a756621212f9
Hermanni Piirainen <hermanni.piirainen@geniem.com>
2023-06-20 14:08:05 +0300

    Revert "Add ManualEvent CPT, model, fields and template"
    This reverts commit f788f7769ca3ede34a4fe9a9c2da491db1e84888.

--- lib/ThemeCustomizationController.php
+++ lib/ThemeCustomizationController.php
@@ -3,10 +3,7 @@
 namespace TMS\Theme\Amuri;
 
 use WP_post;
-use Geniem\ACF\Field;
 use function add_filter;
-use TMS\Theme\Amuri\Taxonomy\ManualEventCategory;
-use TMS\Theme\Amuri\PostType\ManualEvent;
 
 /**
  * Class ThemeCustomizationController
@@ -49,22 +46,6 @@ class ThemeCustomizationController implements \TMS\Theme\Base\Interfaces\Control
 
             return $data;
         } );
-
-        add_filter(
-            'tms/acf/group/fg_page_components/rules',
-            \Closure::fromCallable( [ $this, 'alter_component_rules' ] )
-        );
-
-        add_filter( 'tms/theme/gutenberg/excluded_templates', [ $this, 'excluded_templates' ] );
-
-        add_filter(
-            'tms/theme/layout_events/events',
-            \Closure::fromCallable( [ $this, 'layout_events_events' ] ),
-            10,
-            2
-        );
-
-        add_filter( 'tms/acf/layout/_events/fields', [ $this, 'layout_events_fields' ], 10, 2 );
     }
 
     /**
@@ -206,146 +187,5 @@ class ThemeCustomizationController implements \TMS\Theme\Base\Interfaces\Control
         $link['classes'] = '';
 
         return $link;
-    }
-
-    /**
-     * Hide components from PageCombinedEventsList template.
-     *
-     * @param array $rules ACF group rules.
-     *
-     * @return array
-     */
-    public function alter_component_rules( array $rules ) : array {
-        $rules[] = [
-            'param'    => 'page_template',
-            'operator' => '!=',
-            'value'    => \PageCombinedEventsList::TEMPLATE,
-        ];
-
-        return $rules;
-    }
-
-    /**
-     * Exclude Gutenberg from theme-specific templates.
-     *
-     * @param array $templates The templates array.
-     *
-     * @return array
-     */
-    public function excluded_templates( array $templates ) : array {
-        $templates[] = \PageCombinedEventsList::TEMPLATE;
-
-        return $templates;
-    }
-
-    /**
-     * Filter events for events highlight layout.
-     * Add manual events to the list, sort by start date and return correct amount.
-     *
-     * @param ?array $events The events.
-     * @param array $layout Layout options.
-     * @return array
-     */
-    public function layout_events_events( $events, $layout ) {
-        if ( empty( $events ) ) {
-            $events = [];
-        }
-
-        $curdate      = date( 'Y-m-d' );
-        $start_date   = $layout['starts_today'] ? $curdate : $layout['start'];
-        $start_date   = $start_date ?: $curdate;
-        $end_date     = $layout['end'];
-        $count        = $layout['page_size'] ?: 10;
-        $categories   = $layout['manual_event_categories'] ?? [];
-        $search_query = $layout['text'] ?? '';
-        $args         = [
-            'post_type'      => PostType\ManualEvent::SLUG,
-            'posts_per_page' => $count,
-            'fields'         => 'ids',
-            'meta_query'     => [
-                'relation' => 'AND',
-                [
-                    'key'     => 'start_datetime',
-                    'value'   => $start_date,
-                    'compare' => '>=',
-                ],
-            ],
-        ];
-
-        if ( ! empty( $end_date ) ) {
-            $args['meta_query'][] = [
-                'key'     => 'end_datetime',
-                'value'   => $end_date,
-                'compare' => '<=',
-            ];
-        }
-
-        if ( ! empty( $categories ) ) {
-            $args['tax_query'] = [
-                [
-                    'taxonomy' => Taxonomy\ManualEventCategory::SLUG,
-                    'field'    => 'term_id',
-                    'terms'    => $categories,
-                ],
-            ];
-        }
-
-        if ( ! empty( $search_query ) ) {
-            $args['s'] = $search_query;
-        }
-
-        $query = new \WP_Query( $args );
-
-        echo '<pre>';
-        var_dump( $query );
-        echo '</pre>';
-
-        // Return original events if no manual events found.
-        if ( empty( $query->posts ) ) {
-            return $events;
-        }
-
-        // Normalize the manual events.
-        $manual_events = array_map( function ( $id ) {
-            $event        = (object) get_fields( $id );
-            $event->id    = $id;
-            $event->title = get_the_title( $id );
-            $event->url   = get_permalink( $id );
-            $event->image = has_post_thumbnail( $id ) ? get_the_post_thumbnail_url( $id, 'medium_large' ) : null;
-
-            return ManualEvent::normalize_event( $event );
-        }, $query->posts );
-
-        // Merge manual events with original events.
-        $events = array_merge( $events, $manual_events );
-
-        // Sort events by start datetime objects.
-        usort( $events, function( $a, $b ) {
-            return $a['start_date_raw'] <=> $b['start_date_raw'];
-        } );
-
-        // Return correct amount of events.
-        return array_slice( $events, 0, $count );
-    }
-
-    /**
-     * Filter events layout fields.
-     * Add manual event categories field.
-     *
-     * @param array $fields The fields.
-     * @param string $key The key.
-     * @return array
-     */
-    public function layout_events_fields( $fields, $key ) {
-        $manual_event_categories = ( new Field\Taxonomy( 'Tapahtumakategoriat' ) )
-            ->set_key( "{$key}_manual_event_categories" )
-            ->set_name( 'manual_event_categories' )
-            ->set_instructions( 'Koskee vain manuaalisia tapahtumia.' )
-            ->set_taxonomy( ManualEventCategory::SLUG )
-            ->set_field_type( 'multi_select' );
-
-        $fields[] = $manual_event_categories;
-
-        return $fields;
     }
 }
