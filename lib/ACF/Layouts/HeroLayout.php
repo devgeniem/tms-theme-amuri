e06813a4aee0581158fc6244a102b8feeb70f8b1
Erik Vänttinen <31741222+eebbi@users.noreply.github.com>
2025-06-23 00:18:54 +0300

    TMS-1172: Add video-fields and functionality to hero-component (#65)
    * TMS-1172: Add video-fields and functionality to hero-component
    
    * TMS-1172: Add hero field strings
    
    * TMS-1172: Change hero classes
    
    * TMS-1172: Remove conflicting code for accessibility
    
    * TMS-1172: Change aria-label escaping

--- lib/ACF/Layouts/HeroLayout.php
+++ lib/ACF/Layouts/HeroLayout.php
@@ -48,6 +48,18 @@ class HeroLayout extends Layout {
                 'label'        => 'Kuva',
                 'instructions' => '',
             ],
+            'video'          => [
+                'label'        => 'Videotiedosto',
+                'instructions' => '',
+            ],
+            'video_caption'  => [
+                'label'        => 'Videon tekstivastine',
+                'instructions' => 'Tarkoitettu ruudunlukijoille, ei näytetä sivustolla.',
+            ],
+            'autoplay_video' => [
+                'label'        => 'Käynnistä video heti sivunlatauksessa',
+                'instructions' => '',
+            ],
             'title'         => [
                 'label'        => 'Otsikko',
                 'instructions' => '',
@@ -119,9 +131,28 @@ class HeroLayout extends Layout {
             ->set_key( "${key}_image" )
             ->set_name( 'image' )
             ->set_return_format( 'id' )
-            ->set_wrapper_width( 50 )
             ->set_required()
             ->set_instructions( $strings['image']['instructions'] );
+
+        $video_field = ( new Field\File( $strings['video']['label'] ) )
+            ->set_key( "{$key}_video_file" )
+            ->set_name( 'video_file' )
+            ->set_mime_types( [ 'mp4' ] )
+            ->set_wrapper_width( 33 )
+            ->set_instructions( $strings['video']['instructions'] );
+
+        $video_caption_field = ( new Field\Textarea( $strings['video_caption']['label'] ) )
+            ->set_key( "{$key}_video_caption" )
+            ->set_name( 'video_caption' )
+            ->set_wrapper_width( 33 )
+            ->set_instructions( $strings['video_caption']['instructions'] );
+
+        $autoplay_video_field = ( new Field\TrueFalse( $strings['autoplay_video']['label'] ) )
+            ->set_key( "{$key}_autoplay_video" )
+            ->set_name( 'autoplay_video' )
+            ->use_ui()
+            ->set_wrapper_width( 33 )
+            ->set_instructions( $strings['autoplay_video']['instructions'] );
 
         $title_field = ( new Field\Text( $strings['title']['label'] ) )
             ->set_key( "${key}_title" )
@@ -183,6 +214,9 @@ class HeroLayout extends Layout {
                     'tms/acf/layout/hero--amuri/fields',
                     [
                         $image_field,
+                        $video_field,
+                        $video_caption_field,
+                        $autoplay_video_field,
                         $title_field,
                         $description_field,
                         $link_field,
